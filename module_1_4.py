65af7e9b2d3586278a539e78dbde7fb9a8077754
balvasergiy <balvasergiy@yandex.ru>
2024-09-03 14:33:42 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input() #2nd task
+print(my_string.upper())
+print(my_string.lower())
+length=len(my_string)
+print(length)
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
